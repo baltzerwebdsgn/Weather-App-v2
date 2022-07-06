60c8f84f62c65067aa62a948ed58a8312b17be27
baltzerwebdsgn <nikki@baltzerwebdesign.com>
2022-07-06 18:24:59 -0500

    Added unit indicator change to unit bar buttons
    Buttons are now able to change the indicator symbol upon click
    Added id tag to html to be able to target the letter

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@
 let tempFButton = document.querySelector("#fahrenheit-button");
 let tempCButton = document.querySelector("#celsius-button");
 
+function changeUnitDegree(unit) {
+  let tempUnit = document.querySelector("#current-temp-unit");
+  let rfUnit = document.querySelector("#current-rf-unit");
+  if (unit === "C") {
+    tempUnit.innerHTML = unit;
+    rfUnit.innerHTML = unit;
+  } else {
+    tempUnit.innerHTML = unit;
+    rfUnit.innerHTML = unit;
+  }
+}
+
 function updateCelsiusButton(event) {
   event.preventDefault();
   tempCButton.classList.add("active");
   tempFButton.classList.remove("active");
+  changeUnitDegree("C");
 }
 
 function updateFahrenheitButton(event) {
   event.preventDefault();
   tempCButton.classList.remove("active");
   tempFButton.classList.add("active");
+  changeUnitDegree("F");
 }
 
 tempFButton.addEventListener("click", updateFahrenheitButton);
